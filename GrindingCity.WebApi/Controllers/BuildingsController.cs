2ba5d82e610a01759fc433094fabda58a2f59ed8
f0urth <f0urth.kamil@gmial.com>
2023-03-27 23:48:35 +0200

    Add controller

---
+++ GrindingCity.WebApi/Controllers/BuildingsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrindingCity.WebApi.Controllers;
+
+using Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ApiController]
+[Route("api/[controller]")]
+[SwaggerTag("Buildings")]
+public sealed class BuildingsController : ControllerBase
+{
+    [HttpGet("{id:guid}")]
+    public ActionResult<BuildingDto> Get(Guid id)
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<BuildingDto>> Get()
+    {
+        return Ok();
+    }
+    
+    [HttpPost]
+    public ActionResult<BuildingDto> Post(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        return NoContent();
+    }
+}
