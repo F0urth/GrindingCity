3ac9c092f3ac43004d49929608029c0964a387c1
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:02:17 +0200

    building repo add

---
+++ GrindingCity.Core/Interfaces/Infrastructure/Repositories/IBuildingRepository.cs
@@ -0,0 +1,12 @@
+﻿using GrindingCity.Domain.Models;
+
+namespace GrindingCity.Core.Interfaces.Infrastructure.Repositories
+{
+    public interface IBuildingRepository
+    {
+        public Task<Building> Post(Building building);
+        public Task<Building> GetById(Guid id);
+        public Task<bool> Update(Building building);
+        public Task<bool> Delete(Guid id);
+    }
+}
---
+++ GrindingCity.Infrastructure/Repositories/BuildingRepository.cs
@@ -0,0 +1,60 @@
+﻿using GrindingCity.Core.Interfaces.Infrastructure.Repositories;
+using GrindingCity.Domain.Models;
+using GrindingCity.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrindingCity.Infrastructure.Repositories
+{
+    public class BuildingRepository : IBuildingRepository
+    {
+        private readonly InMemoryDbContext _context;
+
+        public BuildingRepository(InMemoryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Building> Post(Building building)
+        {
+            var result = _context.Buildings.Add(building);
+            await _context.SaveChangesAsync();
+
+            return result.Entity;
+        }
+
+        public async Task<Building> GetById(Guid id)
+        {
+            var result = await FindBuildingById(id);
+
+            return result;
+        }
+
+        public async Task<bool> Update(Building building)
+        {
+            _context.Buildings.Update(building);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var result = await FindBuildingById(id);
+
+            if (result != null)
+            {
+                _context.Buildings.Remove(result);
+                await _context.SaveChangesAsync();
+            }
+
+            return true;
+        }
+
+        private async Task<Building> FindBuildingById(Guid id)
+        {
+            var result = await _context.Buildings.Where(x => x.Id == id).SingleOrDefaultAsync();
+
+            return result;
+        }
+    }
+}
