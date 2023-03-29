5a11960fa4b74dd7e1a343ec20719ca7b6a43b3f
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:02:53 +0200

    update command add

---
+++ GrindingCity.WebApi/DTO/Commands/BuildingCommands/Update/UpdateCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Commands.BuildingCommands.Update
+{
+    public class UpdateCommand : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public UpdateCommand(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ GrindingCity.WebApi/DTO/Commands/BuildingCommands/Update/UpdateCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using GrindingCity.Core.Interfaces.Infrastructure.Repositories;
+using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Commands.BuildingCommands.Update
+{
+    public class UpdateCommandHandler : IRequestHandler<UpdateCommand, bool>
+    {
+        private readonly IBuildingRepository _buildingRepository;
+
+        public UpdateCommandHandler(IBuildingRepository buildingRepository)
+        {
+            _buildingRepository = buildingRepository;
+        }
+
+        public async Task<bool> Handle(UpdateCommand request, CancellationToken cancellationToken)
+        {
+            var building = await _buildingRepository.GetById(request.Id);
+
+            if (building == null)
+            {
+                throw new ArgumentNullException(nameof(building));
+            }
+
+            building.Name = request.Name;
+
+            await _buildingRepository.Update(building);
+
+            return true;
+        }
+    }
+}
