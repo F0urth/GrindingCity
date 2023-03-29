36b015cc2c05574b03ddb21c37428a395e7e84a7
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:02:05 +0200

    in memory db add

---
+++ GrindingCity.Infrastructure/Database/InMemoryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GrindingCity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrindingCity.Infrastructure.Database
+{
+    public class InMemoryDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Building");
+        }
+
+        public DbSet<Building> Buildings { get; set; }
+    }
+}
