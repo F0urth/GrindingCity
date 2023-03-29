82307c7aa40b8fc9079e2955b911fa3d19cb9646
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:03:00 +0200

    delete command add

---
+++ GrindingCity.WebApi/DTO/Commands/BuildingCommands/Delete/DeleteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Commands.BuildingCommands.Delete
+{
+    public class DeleteCommand : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ GrindingCity.WebApi/DTO/Commands/BuildingCommands/Delete/DeleteCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using GrindingCity.Core.Interfaces.Infrastructure.Repositories;
+using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Commands.BuildingCommands.Delete
+{
+    public class DeleteCommandHandler : IRequestHandler<DeleteCommand, bool>
+    {
+        private readonly IBuildingRepository _buildingRepository;
+
+        public DeleteCommandHandler(IBuildingRepository buildingRepository)
+        {
+            _buildingRepository = buildingRepository;
+        }
+
+        public async Task<bool> Handle(DeleteCommand request, CancellationToken cancellationToken)
+        {
+            await _buildingRepository.Delete(request.Id);
+
+            return true;
+        }
+    }
+}
