b6d706fef0797dedbf8cfc7e73d1b8a63b3139c1
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:02:35 +0200

    getById query add

---
+++ GrindingCity.WebApi/DTO/Queries/BuildingQueries/GetById/GetByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using GrindingCity.Domain.Models;
+using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Queries.BuildingQueries.GetById
+{
+    public class GetByIdQuery : IRequest<Building>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ GrindingCity.WebApi/DTO/Queries/BuildingQueries/GetById/GetByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using GrindingCity.Core.Interfaces.Infrastructure.Repositories;
+using GrindingCity.Domain.Models;
+using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Queries.BuildingQueries.GetById
+{
+    public class GetByIdQueryHandler : IRequestHandler<GetByIdQuery, Building>
+    {
+        private readonly IBuildingRepository _buildingRepository;
+
+        public GetByIdQueryHandler(IBuildingRepository buildingRepository)
+        {
+            _buildingRepository = buildingRepository;
+        }
+
+        public async Task<Building> Handle(GetByIdQuery request, CancellationToken cancellationToken)
+        {
+            if (request.Id == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(request.Id));
+            }
+
+            var building = await _buildingRepository.GetById(request.Id);
+
+            return building;
+        }
+    }
+}
