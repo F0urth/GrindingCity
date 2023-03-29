dd9e48b6410cb32f1a8fa316b431130c70b736cb
Marcin <m.pietrucha0@gmail.com>
2023-03-29 20:02:44 +0200

    create command add

---
+++ GrindingCity.WebApi/DTO/Commands/BuildingCommands/Create/CreateCommand.cs
@@ -0,0 +1,15 @@
+﻿using GrindingCity.Domain.Models;
+using MediatR;
+
+namespace GrindingCity.WebApi.DTO.Commands.BuildingCommands.Create
+{
+    public class CreateCommand : IRequest<Building>
+    {
+        public string Name { get; set; }
+
+        public CreateCommand(string name)
+        {
+            Name = name;
+        }
+    }
+}
