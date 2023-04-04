917133f5ac042a17bbc6c9a16c40bb87cb8789b0
Kamil_Kus <Kamil_Kus@epam.com>
2023-04-04 23:42:39 +0200

    Correct delete

--- GrindingCity.Infrastructure/Resources/CommandHandlers/RemoveResourceByIdHandler.cs
+++ GrindingCity.Infrastructure/Resources/CommandHandlers/RemoveResourceByIdHandler.cs
@@ -1,21 +1,22 @@
 ﻿using Domain.Buildings.Providers;
 using Domain.Resources.Commands;
+using Domain.Resources.Providers;
 using MediatR;
 
 namespace GrindingCity.Infrastructure.Resources.CommandHandlers;
 
 public sealed class RemoveResourceByIdHandler : IRequestHandler<RemoveResourceByIdCommand, Unit>
 {
-    private readonly IBuildingRepository _repository;
+    private readonly IResourceRepository _repository;
 
-    public RemoveResourceByIdHandler(IBuildingRepository repository)
+    public RemoveResourceByIdHandler(IResourceRepository repository)
     {
         _repository = repository;
     }
 
     public async Task<Unit> Handle(RemoveResourceByIdCommand command, CancellationToken cancellationToken)
     {
-        await _repository.RemoveBuildingAsync(command.Id);
+        await _repository.RemoveResourceAsync(command.Id);
         return Unit.Value;
     }
 }
--- GrindingCity.Infrastructure/Shared/DependencyInjection.cs
+++ GrindingCity.Infrastructure/Shared/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Buildings.Providers;
+using Domain.Resources.Providers;
 using GrindingCity.Infrastructure.Buildings.Providers;
+using GrindingCity.Infrastructure.Resources.Providers;
 
 namespace GrindingCity.Infrastructure.Shared;
 
@@ -11,7 +13,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddServices(this IServiceCollection collection)
     {
-        return collection.AddScoped<IBuildingRepository, BuildingRepository>()
+        return collection
+            .AddScoped<IBuildingRepository, BuildingRepository>()
+            .AddScoped<IResourceRepository, ResourceRepository>()
             .AddMediatR(e => e.RegisterServicesFromAssemblies(
                 Assembly.GetAssembly(typeof(BuildingEntity))!,
                 Assembly.GetExecutingAssembly()));;
