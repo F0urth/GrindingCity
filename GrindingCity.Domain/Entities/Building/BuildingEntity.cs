9e941c6733529dd59c34548be87e5afde34008b7
Marcin <m.pietrucha0@gmail.com>
2023-04-05 17:50:38 +0200

    building model rename

--- GrindingCity.Domain/Entities/Building/BuildingEntity.cs
+++ GrindingCity.Domain/Entities/Building/BuildingEntity.cs
@@ -1,6 +1,6 @@
-﻿namespace GrindingCity.Domain.Models
+﻿namespace GrindingCity.Domain.Entities.Building
 {
-    public class Building
+    public class BuildingEntity
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
